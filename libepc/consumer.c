801082d43fad1d0ca28b4db2a340f36f249a1736
Mathias Hasselmann <hasselmm@src.gnome.org>
2007-11-19 13:28:59 +0000

    Implement the consumer.
    * libepc/consumer.c, libepc/consumer.h: Really implement the consumer.
    * tests/test-consumer.c: Use the consumer API.
    
    svn path=/trunk/; revision=6

--- libepc/consumer.c
+++ libepc/consumer.c
@@ -1,14 +1,20 @@
 #include "consumer.h"
 
+#include <libsoup/soup-session-sync.h>
+#include <string.h>
+
 enum
 {
-  PROP_NONE
-  /* TODO: fill in properties */
+  PROP_NONE,
+  PROP_HOST,
+  PROP_PORT
 };
 
 struct _EpcConsumerPrivate
 {
-  /* TODO: fill in private members */
+  SoupSession *session;
+  guint16 port;
+  gchar *host;
 };
 
 G_DEFINE_TYPE (EpcConsumer, epc_consumer, G_TYPE_OBJECT);
@@ -16,9 +22,8 @@ G_DEFINE_TYPE (EpcConsumer, epc_consumer, G_TYPE_OBJECT);
 static void
 epc_consumer_init (EpcConsumer *self)
 {
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-                                            EPC_TYPE_CONSUMER,
-                                            EpcConsumerPrivate);
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, EPC_TYPE_CONSUMER, EpcConsumerPrivate);
+  self->priv->session = soup_session_sync_new ();
 }
 
 static void
@@ -31,7 +36,16 @@ epc_consumer_set_property (GObject      *object,
 
   switch (prop_id)
     {
-      /* TODO: handle properties */
+      case PROP_HOST:
+        g_assert (NULL == self->priv->host);
+        self->priv->host = g_value_dup_string (value);
+        break;
+
+      case PROP_PORT:
+        g_assert (0 == self->priv->port);
+        self->priv->port = g_value_get_int (value);
+        g_assert (self->priv->port > 0);
+        break;
 
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -49,7 +63,13 @@ epc_consumer_get_property (GObject    *object,
 
   switch (prop_id)
     {
-      /* TODO: handle properties */
+      case PROP_HOST:
+        g_value_set_string (value, self->priv->host);
+        break;
+
+      case PROP_PORT:
+        g_value_set_int (value, self->priv->port);
+        break;
 
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -62,7 +82,14 @@ epc_consumer_dispose (GObject *object)
 {
   EpcConsumer *self = EPC_CONSUMER (object);
 
-  /* TODO: release resources */
+  if (self->priv->session)
+    {
+      g_object_unref (self->priv->session);
+      self->priv->session = NULL;
+    }
+
+  g_free (self->priv->host);
+  self->priv->host = NULL;
 
   G_OBJECT_CLASS (epc_consumer_parent_class)->dispose (object);
 }
@@ -76,12 +103,84 @@ epc_consumer_class_init (EpcConsumerClass *cls)
   oclass->get_property = epc_consumer_get_property;
   oclass->dispose = epc_consumer_dispose;
 
+  g_object_class_install_property (oclass, PROP_HOST,
+                                   g_param_spec_string ("host", "Host",
+                                                        "Hostname of the publisher service", NULL,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
+                                                        G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK |
+                                                        G_PARAM_STATIC_BLURB));
+  g_object_class_install_property (oclass, PROP_PORT,
+                                   g_param_spec_int ("port", "Port",
+                                                     "TCP/IP port of the publisher service",
+                                                     1, G_MAXUINT16, 80,
+                                                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
+                                                     G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK |
+                                                     G_PARAM_STATIC_BLURB));
+
   g_type_class_add_private (cls, sizeof (EpcConsumerPrivate));
 }
 
 EpcConsumer*
-epc_consumer_new (void)
+epc_consumer_new (const gchar *host,
+                  guint16      port)
 {
-  return g_object_new (EPC_TYPE_CONSUMER, NULL);
+  g_return_val_if_fail (NULL != host, NULL);
+  g_return_val_if_fail (port > 0, NULL);
+
+  return g_object_new (EPC_TYPE_CONSUMER,
+                       "host", host, "port", port,
+                       NULL);
 }
 
+static SoupMessage*
+epc_consumer_create_request (EpcConsumer *self,
+                             const gchar *path)
+{
+  SoupMessage *request;
+  char *request_uri;
+
+  if (NULL == path)
+    path = "/";
+
+  g_assert ('/' == path[0]);
+
+  request_uri = g_strdup_printf ("http://%s:%d/%s", self->priv->host,
+                                 self->priv->port, path + 1);
+  request = soup_message_new ("GET", request_uri);
+  g_free (request_uri);
+
+  return request;
+}
+
+gchar*
+epc_consumer_lookup (EpcConsumer *self,
+                     const gchar *key,
+                     gsize       *length)
+{
+  SoupMessage *request = NULL;
+  gchar *contents = NULL;
+  gchar *path = NULL;
+  gint status = 0;
+
+  g_return_val_if_fail (EPC_IS_CONSUMER (self), NULL);
+  g_return_val_if_fail (NULL != key, NULL);
+
+  path = g_strconcat ("/get/", key, NULL);
+  request = epc_consumer_create_request (self, path);
+  g_free (path);
+
+  status = soup_session_send_message (self->priv->session, request);
+
+  if (SOUP_STATUS_OK == status)
+    {
+      if (length)
+        *length = request->response.length;
+
+      contents = g_malloc (request->response.length);
+      memcpy (contents, request->response.body,
+              request->response.length);
+    }
+
+  g_object_unref (request);
+  return contents;
+}
--- tests/test-consumer.c
+++ tests/test-consumer.c
@@ -1,9 +1,9 @@
+#include "libepc/consumer.h"
 #include "libepc/publisher.h"
 
-#include <gtk/gtk.h>
 #include <avahi-ui/avahi-ui.h>
-#include <libsoup/soup-message.h>
-#include <libsoup/soup-session-sync.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
 
 static void
 show_error (const char *message, ...)
@@ -25,21 +25,37 @@ show_error (const char *message, ...)
 }
 
 static void
-show_response (SoupMessage *message)
+show_value (const gchar *publisher,
+            const gchar *key,
+            const gchar *value,
+            gsize        length)
 {
   GtkTextBuffer *buffer;
+
   GtkWidget *text_view;
   GtkWidget *scroller;
   GtkWidget *dialog;
+  GtkWidget *label;
 
-  dialog = gtk_dialog_new_with_buttons ("Glom Configuration", NULL,
+  gchar *markup;
+
+  dialog = gtk_dialog_new_with_buttons ("Published Value", NULL,
                                         GTK_DIALOG_NO_SEPARATOR,
                                         GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE,
                                         NULL);
 
+  markup = g_markup_printf_escaped (
+    "Value stored for <b>%s</b> at <b>%s</b> (%d %s):",
+    key, publisher, length, ngettext ("byte", "bytes", length));
+
+  label = gtk_label_new (NULL);
+  gtk_label_set_markup (GTK_LABEL (label), markup);
+  gtk_misc_set_padding (GTK_MISC (label), 6, 6);
+  gtk_widget_show (label);
+  g_free (markup);
+
   buffer = gtk_text_buffer_new (NULL);
-  gtk_text_buffer_set_text (buffer, message->response.body,
-                                    message->response.length);
+  gtk_text_buffer_set_text (buffer, value, length);
 
   text_view = gtk_text_view_new_with_buffer (buffer);
 
@@ -53,6 +69,8 @@ show_response (SoupMessage *message)
   gtk_widget_show_all (scroller);
 
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
+                      label, FALSE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
                       scroller, TRUE, TRUE, 0);
 
   gtk_window_set_default_size (GTK_WINDOW (dialog), 500, 350);
@@ -60,56 +78,40 @@ show_response (SoupMessage *message)
   gtk_widget_destroy (dialog);
 }
 
-static SoupMessage*
-build_document_request (const char *host,
-                        int         port,
-                        const char *path)
-{
-  SoupMessage *msg;
-  char *uri;
-
-  g_return_val_if_fail (port > 0 && port < 65536, NULL);
-  g_return_val_if_fail (NULL != host, NULL);
-  g_return_val_if_fail (NULL != path, NULL);
-
-  if (NULL == path)
-    path = "/";
-
-  g_return_val_if_fail ('/' == path[0], NULL);
-
-  uri = g_strdup_printf ("http://%s:%d/%s", host, port, path + 1);
-  msg = soup_message_new ("GET", uri);
-
-  g_free (uri);
-
-  return msg;
-}
-
 static void
-authenticate_cb (SoupSession  *session G_GNUC_UNUSED,
-                 SoupMessage  *message G_GNUC_UNUSED,
-                 gchar        *auth_type G_GNUC_UNUSED,
-                 gchar        *auth_realm G_GNUC_UNUSED,
-                 gchar       **username,
-                 gchar       **password,
-                 gpointer      data G_GNUC_UNUSED)
+lookup_value (EpcConsumer *consumer,
+              const gchar *key,
+              const gchar *publisher)
 {
-  *username = g_strdup (g_get_user_name ());
-  *password = g_strdup ("yadda");
+  gchar *value;
+  gsize length;
+
+  value = epc_consumer_lookup (consumer, key, &length);
+
+  if (value)
+    show_value (publisher, key, value, length);
+  else
+    show_error ("<b>Value not found.</b>\n\n"
+                "No value found for key <b>%s</b> at <b>%s</b>.",
+                key, publisher);
+
+  g_free (value);
 }
 
 int
 main (int   argc,
       char *argv[])
 {
+  EpcConsumer *consumer = NULL;
+  gchar *publisher = NULL;
   GtkWidget *dialog;
+  int i;
 
   g_thread_init (NULL);
   gdk_threads_init ();
   gtk_init (&argc, &argv);
 
-  dialog = aui_service_dialog_new ("Choose an Easy Publish and Commit Service",
-				   NULL,
+  dialog = aui_service_dialog_new ("Choose an Easy Publish and Consume Service", NULL,
                                    GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
                                    GTK_STOCK_CONNECT, GTK_RESPONSE_ACCEPT,
                                    NULL);
@@ -120,34 +122,26 @@ main (int   argc,
 
   if (GTK_RESPONSE_ACCEPT == gtk_dialog_run (GTK_DIALOG (dialog)))
     {
-      SoupSession *session;
-      SoupMessage *request;
-      guint status;
-
-      session = soup_session_sync_new ();
-
-      g_signal_connect (session, "authenticate",
-                        G_CALLBACK (authenticate_cb), NULL);
-
-      request = build_document_request (
-        aui_service_dialog_get_host_name (AUI_SERVICE_DIALOG (dialog)),
-        aui_service_dialog_get_port (AUI_SERVICE_DIALOG (dialog)),
-        "/get/source-code");
-
-      status = soup_session_send_message (session, request);
-
-      if (SOUP_STATUS_OK == status)
-        show_response (request);
-      else
-        show_error ("<b>Cannot retreive configuration file:</b>\n\n"
-                    "Server response was `%s' (%d).",
-                    soup_status_get_phrase (status), status);
-
-      g_object_unref (request);
-      g_object_unref (session);
+      const gint port = aui_service_dialog_get_port (AUI_SERVICE_DIALOG (dialog));
+      const gchar *host = aui_service_dialog_get_host_name (AUI_SERVICE_DIALOG (dialog));
+      publisher = g_strdup (aui_service_dialog_get_service_name (AUI_SERVICE_DIALOG (dialog)));
+      consumer = epc_consumer_new (host, port);
     }
 
   gtk_widget_destroy (dialog);
+
+  if (consumer)
+    {
+      if (argc > 1)
+        for (i = 1; i < argc; ++i)
+          lookup_value (consumer, argv[i], publisher);
+      else
+        lookup_value (consumer, "source-code", publisher);
+
+      g_object_unref (consumer);
+    }
+
+  g_free (publisher);
 
   return 0;
 }
