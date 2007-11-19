c6148343b97b7cbbf26b64be1d93c52b59c2175b
Mathias Hasselmann <hasselmm@src.gnome.org>
2007-11-19 13:30:24 +0000

    Always dump the test cases' stdout and stderr when EPC_DEBUG is set.
    * tests/test-runner.sh: Consider EPC_DEBUG variable.
    
    svn path=/trunk/; revision=49

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+test -n "$EPC_DEBUG" && echo "$0: running $1..."
 "$@" 2> "$1.err" > "$1.out"
 result=$?
 
-test $result -ne 0 && cat "$1.err"
+if test -n "$EPC_DEBUG"
+then
+  echo "===== BEGIN OF STDOUT ====="
+  cat "$1.out"
+  echo "===== END OF STDOUT ====="
+
+  echo "===== BEGIN OF STDERR ====="
+  cat "$1.out"
+  echo "===== END OF STDERR ====="
+elif test $result -ne 0
+then
+  cat "$1.err"
+fi
+
 exit $result
